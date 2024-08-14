dfb69a501aa78a8916af4bf7f97c4f9ecc223ee4
Felipe Trebino <fliphd1505@gmail.com>
2024-08-14 18:42:20 -0300

    Update Main.java

--- CineLumiere/src/main/java/main/Main.java
+++ CineLumiere/src/main/java/main/Main.java
@@ -412,17 +412,15 @@ public class Main {
         usuarios.add(cliente);
 
         Filme filme1 = new Filme("A Mosca",96,"O excêntrico cientista Seth Brundle completa seu dispositivo de teletransporte e decide testar a eficácia do experimento em si próprio. Sem que ele perceba, uma mosca cai no aparelho durante o processo e provoca uma fusão dele com o inseto. Contudo, ele pensa que o experimento foi um sucesso, até que começa a notar que as células da mosca estão tomando conta de seu corpo, e ele acaba se transformando em uma criatura monstruosa.", "David Cronenberg","Jeff Goldblum, Geena Davis", 1986,"14 anos", "EUA");
-        filme1.exibirDetalhes();
-
+        
         Documentario documentario1 = new Documentario("Paris is Burning",78,"Uma crônica da cena drag queen nova-iorquina dos anos 1980, focando nos bailes, dança vogue, ambições e sonhos daqueles que proporcionaram calor e vitalidade para uma era.","Jennie Livingston", 1990,"18 anos", "EUA");
-        documentario1.exibirDetalhes();
-
+        
         midias.add(filme1);
         midias.add(documentario1);
 
         Sessao sessao1 = new Sessao(sala1,filme1, LocalDate.of(2024,07,31), LocalTime.of(16,40));
         sala1.adicionarSessao(sessao1);
-        sala1.exibirDetalhes();
+      
     }
 
     private static void exibirMenuUsuarios(Scanner scanner) {
